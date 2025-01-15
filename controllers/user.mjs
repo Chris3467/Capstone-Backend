793efcb411185f2347969cb759e0c6631db74db5
Chris3467 <chris.collado815@gmail.com>
2025-01-15 14:57:39 -0500

    updated

--- controllers/user.mjs
+++ controllers/user.mjs
@@ -1,15 +1,21 @@
 import User from "../models/user.mjs";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 // Create a new user
 async function create(req, res) {
   try {
     const createdUser = await User.create(req.body);
-    res.status(201).json({
-      message: "User created successfully",
-      user: createdUser,
-    });
+    const token = createJWT(createdUser);
+    res
+      .status(201)
+      .json({
+        token: token,
+        message: "User created successfully",
+        user: createdUser,
+      });
   } catch (err) {
+    console.log(err);
     res.status(400).json({
       error: "Failed to create user",
       details: err.message,
@@ -22,15 +28,28 @@ async function create(req, res) {
 async function login(req, res) {
   try {
     const user = await User.findOne({ email: req.body.email });
+    console.log(user);
     if (!user) throw new Error("User not found");
     // * if we find the user, compare the password, but it is stored encrypted
     // * 1st argument is from the credentials that the user typed in
     // * 2nd argument is what is stored in the database
     const match = await bcrypt.compare(req.body.password, user.password);
     if (!match) throw new Error("Invalid password");
+    const token = createJWT(user);
+    res.json(token);
   } catch (err) {
+    console.log(err);
     res.status(400).json(err);
   }
 }
 
-export default { create, login };
+// function to create JTW for users
+function createJWT(user) {
+  return jwt.sign(
+    // data payload
+    { user },
+    process.env.SECRET,
+    { expiresIn: "24h" }
+  );
+}
+export default { create, login, createJWT };
